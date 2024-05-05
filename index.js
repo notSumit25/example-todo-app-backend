cf551a95c1789d8fb028b0d1195a33f412543f65
Sumit <sumitgarg380@gmail.com>
2024-05-05 18:32:15 +0530

    Routes

--- index.js
+++ index.js
@@ -1,29 +1,44 @@
 const express = require('express');
 const { createTodo, updateTodo } = require('./types');
+const { todo } = require('./db');
 const app = express();
 app.use(express.json());
 
 
-app.post('/todo', (req, res) => {
+app.post('/todo', async (req, res) => {
   const createPayload = req.body;
   const parsedPayload = createTodo.safeParse(createPayload);
   if(!parsedPayload.success) {
     return res.status(400).send(parsedPayload.error.errors);
   }
+  await todo.create({
+    title:createPayload.title,
+    description:createPayload.description,
+    
+  });
+    res.send('Todo created');
 
 });
 
 
-app.get('/todos', (req, res) => {
-  res.send('Get all todos');
+app.get('/todos', async (req, res) => {
+    const todos = await todo.find();
+    res.send(todos);
 });
 
 
-app.put('/completed', (req, res) => {
+app.put('/completed', async (req, res) => {
     const updatePayload = req.body;
     const parsedPayload = updateTodo.safeParse(updatePayload);
     if(!parsedPayload.success) {
       return res.status(400).send(parsedPayload.error.errors);
     }
+
+    todo.updateOne({ _id: req.body.id }, { completed: true }, (err) => {
+        if(err) {
+            return res.status(500).send('Something went wrong');
+        }
+        res.send('Todo updated');
+    });
     });
 
